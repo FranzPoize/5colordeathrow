daf9f9a060d66522890cc7b549e485771950c880
Francois <francois.poizat@gmail.com>
2013-11-22 23:05:04 +0100

    added color object

--- src/gameFP.js
+++ src/gameFP.js
@@ -2,9 +2,17 @@ window.onload = function() {
 	Crafty.init(500,350, document.getElementById('game'));
 
 	Crafty.scene('testCollision',function() {
-		Crafty.background('#000');
+		Crafty.background('#999');
 
-		Crafty.e('2D, Canvas, Color, MoveTo').attr({x:0,y:0,h:40,w:40}).color('#f00');
+		Crafty.e('2D, Canvas, Color, MoveTo, Collision ,WiredHitBox')
+			.attr({x:0,y:0,h:40,w:40})
+			.color(color.five);
+			.collision(new Crafty.polygon([0,0],[45,0],[45,45],[0,45]));
+
+		Crafty.e('2D, Canvas, Color, Enemy, Collision ,WiredHitBox')
+			.attr({x:100,y:100,h:40,w:40})
+			.color('#ff0')
+			.collision(new Crafty.polygon([0,0],[45,0],[45,45],[0,45]));;
 	})
 
 	Crafty.scene('testCollision');
