161612593b26b712c1fbc601a2bec7ca7ac74670
tdubourg <plop@plop.fake>
2013-11-23 14:59:29 +0100

    Boost bar is now updating itself when needed + animations !

--- src/boost.js
+++ src/boost.js
@@ -1,26 +1,27 @@
 "use strict"
 
+var PROGRESS_BAR_MAX_VALUE_AND_BOOST_THRESHOLD = 200
+
 var l = function (a) {
 	if (console && console.log) {
 		console.log(a)
 	};
 };
 
-// Temp
-var Score = 10
 Crafty.bind('SceneChange', function (data) {
-	l("HEYHO" + data.newScene)
 	if (data.newScene != 'game') {
 		return
 	};
-	l("HEYHO")
 	var progressBar = Crafty.e("2D, DOM, ProgressBar")
-    .attr({ x: 200, y : Crafty.DOM.window.height - 35, w: 300, h: 25})
-    .progressBar(100, false, "blue", "green")
-    .updateBarProgress(Score);
-    setInterval(function () {
-    	l("BLORG")
-    	Score += 10
-    	progressBar.updateBarProgress(Score);
-    }, 1000)
+	.attr({ x: 200, y : Crafty.DOM.window.height - 35, w: 300, h: 25})
+	.progressBar(PROGRESS_BAR_MAX_VALUE_AND_BOOST_THRESHOLD, false, "blue", ["green", "magenta", "white", "cyan", "red", "orange", "gray"])
+	.updateBarProgress(0);
+
+	Crafty.bind('CollisionSameColor', function (data) {
+		progressBar.updateBarProgress(progressBar.currentProgress() + data.enemy.getScore(true))
+	})
+
+	Crafty.bind('CollisionOtherColor', function (data) {
+		progressBar.updateBarProgress(progressBar.currentProgress() - data.enemy.getScore(true))
+	})
 });
