6b4b2795b644cdbf41c56751a04bf1cc9a587e11
tdubourg <plop@plop.fake>
2013-11-24 15:42:22 +0100

    Level14 finished?

--- src/levels/level14_intro_snake.js
+++ src/levels/level14_intro_snake.js
@@ -14,7 +14,7 @@ window.levels.level14 = {
 			chunkW: 32,
 			speedX: 2,
 			speedY: 2,
-			detectionDistance: 200,
+			detectionDistance: 500,
 			scoreType:'SnakeEnemy',
 			behavior: {}
 		}, {
@@ -46,15 +46,15 @@ window.levels.level14 = {
 			scoreType:'DotEnemy',
 			behavior: {}
 		}],
-		15: [{
+		10: [{
 			type: 'snake',
 			color: 'two',
 			coords: [200,100],
-			chunksNumber: 7,
-			chunkH: 30,
-			chunkW: 30,
-			speedX: 7,
-			speedY: 7,
+			chunksNumber: 4,
+			chunkH: 32,
+			chunkW: 32,
+			speedX: 3.5,
+			speedY: 3.5,
 			detectionDistance: 200,
 			scoreType:'SnakeEnemy',
 			behavior: {}
@@ -122,6 +122,64 @@ window.levels.level14 = {
 			color: 'two',
 			lifespan: 20,
 			coords: [1000,500],
+			scoreType:'DotEnemy',
+			behavior: {}
+		}],
+		15: [{
+			type: 'snake',
+			color: 'five',
+			coords: [200, 100],
+			chunksNumber: 6,
+			chunkH: 32,
+			chunkW: 32,
+			speedX: 7,
+			speedY: 7,
+			detectionDistance: 100,
+			scoreType:'SnakeEnemy',
+			behavior: {}
+		}, {
+			type: 'dot',
+			color: 'three',
+			lifespan: 20,
+			coords: [800,100],
+			scoreType:'DotEnemy',
+			behavior: {}
+		},{
+			type: 'dot',
+			color: 'one',
+			lifespan: 20,
+			coords: [900,400],
+			scoreType:'DotEnemy',
+			behavior: {}
+		},{
+			type: 'dot',
+			color: 'two',
+			lifespan: 20,
+			coords: [100,400],
+			scoreType:'DotEnemy',
+			behavior: {}
+		},{
+			type: 'shape',
+			color: 'three',
+			coords: [0,100],
+			width: 1250,
+			height: 10,
+			scoreType:'ComboEnemy',
+			behavior: {
+				type:'Translation',
+				orient:'vert'}
+		},{
+			type: 'dot',
+			color: 'one',
+			lifespan: 20,
+			coords: [1000,200],
+			scoreType:'DotEnemy',
+			behavior: {}
+		},{
+			type: 'dot',
+			color: 'two',
+			lifespan: 20,
+			coords: [400,200],
 			scoreType:'DotEnemy',
 			behavior: {}
 		}]
