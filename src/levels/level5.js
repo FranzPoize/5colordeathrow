537a51765a3ec3d8fd8b9ae43c00e3653cabf89f
Francois <francois.poizat@gmail.com>
2013-11-24 12:02:03 +0100

    lvl 5

---
+++ src/levels/level5.js
@@ -0,0 +1,181 @@
+'use strict';
+
+window.levels.level5 = {
+	id: 5, 
+	name:'lvl 5',
+	duration: 30,
+	score: 1337, 
+	events: {
+		5: [{
+			type: 'dot',
+			color: 'one',
+			lifespan: 20,
+			coords: [100,100],
+			scoreType:'DotEnemy',
+			behavior: {	}
+		},{
+			type: 'dot',
+			color: 'one',
+			lifespan: 20,
+			coords: [100,150],
+			scoreType:'DotEnemy',
+			behavior: {	}
+		}],
+		6: [{
+			type: 'dot',
+			color: 'two',
+			lifespan: 20,
+			coords: [150,100],
+			scoreType:'DotEnemy',
+			behavior: {	}
+		},{
+			type: 'dot',
+			color: 'two',
+			lifespan: 20,
+			coords: [150,150],
+			scoreType:'DotEnemy',
+			behavior: {	}
+		}],
+		7: [{
+			type: 'dot',
+			color: 'three',
+			lifespan: 20,
+			coords: [200,100],
+			scoreType:'DotEnemy',
+			behavior: {	}
+		},{
+			type: 'dot',
+			color: 'three',
+			lifespan: 20,
+			coords: [200,150],
+			scoreType:'DotEnemy',
+			behavior: {	}
+		}],
+		8: [{
+			type: 'dot',
+			color: 'four',
+			lifespan: 20,
+			coords: [250,100],
+			scoreType:'DotEnemy',
+			behavior: {	}
+		},{
+			type: 'dot',
+			color: 'four',
+			lifespan: 20,
+			coords: [250,150],
+			scoreType:'DotEnemy',
+			behavior: {	}
+		}],
+		9: [{
+			type: 'dot',
+			color: 'five',
+			lifespan: 20,
+			coords: [300,100],
+			scoreType:'DotEnemy',
+			behavior: {	}
+		},{
+			type: 'dot',
+			color: 'five',
+			lifespan: 20,
+			coords: [300,150],
+			scoreType:'DotEnemy',
+			behavior: {	}
+		}],
+		10: [{
+			type: 'dot',
+			color: 'four',
+			lifespan: 20,
+			coords: [350,100],
+			scoreType:'DotEnemy',
+			behavior: {	}
+		},{
+			type: 'dot',
+			color: 'four',
+			lifespan: 20,
+			coords: [350,150],
+			scoreType:'DotEnemy',
+			behavior: {	}
+		}],
+		11: [{
+			type: 'dot',
+			color: 'three',
+			lifespan: 20,
+			coords: [400,100],
+			scoreType:'DotEnemy',
+			behavior: {	}
+		},{
+			type: 'dot',
+			color: 'three',
+			lifespan: 20,
+			coords: [400,150],
+			scoreType:'DotEnemy',
+			behavior: {	}
+		}],
+		12: [{
+			type: 'dot',
+			color: 'two',
+			lifespan: 20,
+			coords: [450,100],
+			scoreType:'DotEnemy',
+			behavior: {	}
+		},{
+			type: 'dot',
+			color: 'two',
+			lifespan: 20,
+			coords: [450,150],
+			scoreType:'DotEnemy',
+			behavior: {	}
+		},{
+			type: 'dot',
+			color: 'two',
+			lifespan: 20,
+			coords: [1000,450],
+			scoreType:'DotEnemy',
+			behavior: {
+				type:'Follower'
+			}
+		},{
+			type: 'dot',
+			color: 'two',
+			lifespan: 20,
+			coords: [450,150],
+			scoreType:'DotEnemy',
+			behavior: {
+				type:'Follower'
+			}
+		},{
+			type: 'dot',
+			color: 'two',
+			lifespan: 20,
+			coords: [450,100],
+			scoreType:'DotEnemy',
+			behavior: {
+				type:'Follower'
+			}
+		},{
+			type: 'dot',
+			color: 'two',
+			lifespan: 20,
+			coords: [450,150],
+			scoreType:'DotEnemy',
+			behavior: {
+				type:'Follower'
+			}
+		}],
+		13: [{
+			type: 'dot',
+			color: 'one',
+			lifespan: 20,
+			coords: [500,100],
+			scoreType:'DotEnemy',
+			behavior: {	}
+		},{
+			type: 'dot',
+			color: 'one',
+			lifespan: 20,
+			coords: [500,150],
+			scoreType:'DotEnemy',
+			behavior: {	}
+		}]
+	}
+};
