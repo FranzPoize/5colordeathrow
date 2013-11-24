c137a428eef73ce010989623c843dbd52254e6ee
louisremi <lrbabe@gmail.com>
2013-11-24 16:11:46 +0100

    woops

--- src/components/Level.js
+++ src/components/Level.js
@@ -163,7 +163,7 @@ Crafty.c('Level', {
 							w: 400,
 							h: 40
 						})
-						.text( Crafty('Score')._text + ' / ' + levelData.score )
+						.text( Crafty('Score')._text )
 						.textColor('#ff6600')
 						.textFont({ size: '40px', weight: 'bold'})
 						.queue([{
